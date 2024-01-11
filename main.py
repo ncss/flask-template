1ed60c3416e125b4c300eb5ad3cd69245c24ad00
Joel Gibson <joel@jgibson.id.au>
2024-01-11 23:42:51 +1100

    Add EchoBot template before project

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import pprint
+
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.post('/echo')
+def echo_bot():
+    message = request.json
+    print(f"\nIncoming message to {request.path}:")
+    pprint.pprint(message, indent=2)
+
+    message_text = message['text']
+    return {
+        'author': 'EchoBot',
+        'text': f"Hello! Your message was: {message_text}"
+    }
 
 
 if __name__ == "__main__":
