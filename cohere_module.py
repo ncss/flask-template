6e3ddf4914a1e7f4e1442d57b69f8654954c44e2
Serpyne <156378996+Serpyne@users.noreply.github.com>
2024-01-17 12:32:24 +0000

    Update 14 files

--- cohere_module.py
+++ cohere_module.py
@@ -1,16 +1,35 @@
 from langchain_community.chat_models import ChatCohere
 from langchain_core.messages import HumanMessage
 from langchain_community.embeddings import CohereEmbeddings
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
+
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain
 
 load_dotenv()
 
-llm = ChatCohere(model="command", temperature=0.3)
-embeddings = CohereEmbeddings(mode="")
+initial_prompt = open("./static/prompt.txt", "r").read()
 
-prompt = HumanMessage(
-    content=""
-)
-message = "Make a rap"
+class ChatBot:
+    def __init__(self, *args, **kwargs):
 
-print(llm.invoke(message))
+        self.llm = ChatCohere(*args, **kwargs)
+        self.history = [
+            {"user_name": "User", "text": initial_prompt},
+            {"user_name": "System", "text": "Yes."}
+        ]
+
+    def chat_invoke(self, message):
+
+        self.history.append({"user_name": "User", "text": message})
+
+        response = self.llm.invoke(
+            input=message,
+            chat_history=self.history
+        )
+
+        self.history.append({"user_name": "System", "text": response.content})
+
+        return response
+        # return self.llm.invoke(message).content
